80c98f53c3969d05438d66ca0d9c70d226e23b94
Konstatnit <yakovlev.kl@yandex.ru>
2021-11-23 13:57:38 +0500

    Работа по списку задач проекта: 2 Собери Maven проект: подключи Jacoco, Mockito и JUnit. -- ОК 3 Изолируй класс Lion от класса Feline. -- ОК

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -2,9 +2,29 @@ package com.example;
 
 import java.util.List;
 
-public class Lion {
+public class Lion extends Animal implements Predator {
 
     boolean hasMane;
+
+    @Override
+    public List<String> eatMeat() throws Exception {
+        return getFood("Хищник");
+    }
+
+    @Override
+    public String getFamily() {
+        return "Кошачьи";
+    }
+
+    @Override
+    public int getKittens() {
+        return getKittens(1);
+    }
+
+    @Override
+    public int getKittens(int kittensCount) {
+        return kittensCount;
+    }
 
     public Lion(String sex) throws Exception {
         if ("Самец".equals(sex)) {
@@ -16,17 +36,8 @@ public class Lion {
         }
     }
 
-    Feline feline = new Feline();
-
-    public int getKittens() {
-        return feline.getKittens();
-    }
-
     public boolean doesHaveMane() {
         return hasMane;
     }
 
-    public List<String> getFood() throws Exception {
-        return feline.getFood("Хищник");
-    }
 }
