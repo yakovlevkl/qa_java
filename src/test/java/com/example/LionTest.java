6a336e6c1a14107f5e213712a95561c8a26de7db
Konstatnit <yakovlev.kl@yandex.ru>
2021-11-23 17:04:15 +0500

    additional cases: 1. Cоздай класс льва Алекса из мультфильма «Мадагаскар». 2. Покрой тестами созданный класс. 3. Попробуй покрыть 100% кода тестами.

--- src/test/java/com/example/LionTest.java
+++ src/test/java/com/example/LionTest.java
@@ -7,7 +7,7 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 public class LionTest {
-        private Lion lion = new Lion("Самка");
+    private Lion lion = new Lion("Самка");
 
     public LionTest() throws Exception {
     }
